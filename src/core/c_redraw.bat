abfab1b52a83f6cfbe65dd09a0a9e006efe59616
Phu Minh (FreakC) <53084840+nguyenphuminh@users.noreply.github.com>
2021-04-12 16:20:17 +0700

    Add files via upload

---
+++ src/core/c_redraw.bat
@@ -0,0 +1,17 @@
+for /L %%A in (1,1,22) do (
+	set x%%Ay1=%floorinit%
+	set x%%Ay2=%floorinit%
+	set x%%Ay3=%floorinit%
+	set x%%Ay4=%floorinit%
+	set x%%Ay5=%floorinit%
+	set x%%Ay6=%floorinit%
+	set x%%Ay7=%floorinit%
+	set x%%Ay8=%floorinit%
+	set x%%Ay9=%floorinit%
+	set x%%Ay10=%floorinit%
+	set x%%Ay11=%floorinit%
+	set x%%Ay12=%floorinit%
+	set x%%Ay13=%floorinit%
+	set x%%Ay14=%floorinit%
+	set x%%Ay15=%floorinit%
+)
